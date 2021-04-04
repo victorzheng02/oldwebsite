295ddfed170ae6ae0960caa1c7e180d31e615f71
SmilestheSad <vzheng@gmail.com>
2021-04-04 19:23:20 -0400

    changed scrollbar

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Footer extends React.Component{
     render(){
         return(
-            <h3 id="footer">Created by Victor Zheng<br></br>Last updated Jan 2021</h3>
+            <h3 id="footer">Created by Victor Zheng<br></br>Last updated Apr 2021</h3>
         )
     }
 }
